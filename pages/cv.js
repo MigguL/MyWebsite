301d3b6232e40c9bd27debf1e8bff1e80df0d648
Michal Latos <mlrapid999@gmail.com>
2020-04-17 11:00:15 +0200

    Initial commit

---
+++ pages/cv.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import BaseLayout from '../components/layouts/BaseLayout';
+class Cv extends React.Component{
+
+
+    render() {
+        {
+          return (
+            <BaseLayout>
+              <h1>I am Cv Page</h1>
+            </BaseLayout>
+          )
+        }
+    }
+  }
+
+export default Cv;
---
+++ pages/portfolios.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import axios from 'axios';
+import Link from 'next/link';
+import BaseLayout from '../components/layouts/BaseLayout';
+class Portfolios extends React.Component{
+
+  static async getInitialProps(){
+    let posts = [];
+
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      posts = response.data;
+    }catch(err){
+      console.log(err);
+    }
+    return {posts: posts.splice(0,10)}; 
+  }
+
+  renderPosts(posts) {
+    return posts.map((post, index) => {
+      return (
+        <li key={index}>
+          <Link as={`/portfolio/${post.id}`} href="/portfolio/[id]">
+            <a style={{'fontSize': '20px'}}>{post.title}</a>
+          </Link>
+        </li>
+      )
+    })
+  }
+
+    render() {
+        {
+      const {posts} = this.props;
+
+          return (
+            <BaseLayout>
+              <h1>I am Portfolio Page</h1>
+              <ul>
+                {this.renderPosts(posts)}
+              </ul>
+            </BaseLayout>
+            
+          )
+        }
+    }
+  }
+
+export default Portfolios;
